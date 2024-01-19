6ed332958650a6084536e2955e16ef0c4d337292
fuad1706 <fuadshorlarxwix@gmail.com>
2024-01-19 14:26:18 -0800

    java

---
+++ tut.js
@@ -0,0 +1,220 @@
+// // Data Types:
+// // undefined, null, boolean, string, symbol, number, and object
+
+// //var myName = "Beau";
+
+// //myName = 8;
+
+// //let ourName = "freeCodeCamp";
+
+// //const pi = 3.14;
+
+// // variable is like a container used to store data
+
+// // three ways to declare a variable in javascript
+
+// // var - allows to be used through out the whole programme
+
+// // let - will only be used in the scope where you declare it
+
+// //const - is avariable that can never chnage if change it will return error
+
+// // variable declaration
+// // var a;
+
+// // variable assigning
+// // var b = 2;
+
+// //console.log allows you to see things in the console
+
+// //Initialize these three variables
+
+// // var a = 5;
+// //var b = 10;
+// //var c = "I am a"
+
+// // Do not change code below this line
+
+// // a = a + 1;
+// //b = b + 5;
+// //c = c + "string";
+
+// // console.log(a);
+// //console.log(b);
+// //console.log(c);
+
+// // Declarations is case sensitive it is advisable to make use
+// // camelCase when declaring variables
+// //var studlyCapVar;
+// //var properCamelCase;
+// //var titleCaseOver;
+
+// // Assignments
+// //studlyCapVar = 10;
+// //properCamelCase = "A String"
+// //titleCaseOver = 9000;
+
+// // Adding numbers
+
+// //var sum = 10 + 10;
+// //console.log(sum);
+
+// // Substracting number
+
+// //var difference = 45 - 33;
+// //console.log(difference);
+
+// // multiplication
+// //var product = 8 * 10;
+// //console.log(product);
+
+// // Division
+// //var quotient = 66 / 33;
+// //console.log(quotient);
+
+// // incrmenting number
+// //var myVar = 87;
+// //myVar++
+// //console.log(myVar)
+
+// // Decrementing numbers
+// //var myVar = 11 ;
+// //myVar--;
+// //console.log(myVar);
+
+// // compound assignment with augmented addition
+// //var a = 3;
+// //var b = 17;
+// //var c = 12;
+
+// //a += 12;
+// //b += 9;
+// //c += 7;
+// //console.log(a)
+// //console.log(b)
+// //console.log(c)
+
+// // compound assignment with augmented substraction
+// //var a = 11;
+// //var b = 9;
+// //var c = 3;
+
+// //a -= 6;
+// //b -= 15;
+// //c -= 1;
+// //console.log(a)
+// //console.log(b)
+// //console.log(c)
+
+// // compound assignment with augmented multiplication
+
+// //var a = 5;
+// //var b = 12;
+// // var c = 4.6;
+
+// // a *= 5;
+// // b *= 3;
+// // c *= 10;
+
+// // console.log(a)
+// // console.log(b)
+// // console.log(c)
+
+// //compound assignment with augmented division
+// var a = 48;
+// var b = 108;
+// var c = 33;
+
+// a /= 12;
+// b /= 4;
+// c /= 11;
+
+// // console.log(a)
+// console.log(b)
+// console.log(c)
+
+// declare string variable.. single or double quote or batic
+
+// var firstName = "Alan";
+// var lastName = "Turing";
+
+// var myFirstName = "Beau";
+// var myLastName = "Carnes"
+
+// escaping literal quotes in strings by putting a back slash
+
+//var myStr = "I am a \"double quoted\" string inside \"double quotes"
+//console.log(myStr);
+
+// Quoting strings with single quotes
+// in some you can also use single quote in place of back slash
+
+// Escape Sequence in strings
+/*****
+CODE OUTPUT
+\' single quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+
+*****/
+
+//var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+//console.log(myStr)
+
+//concatenating strings with plus operator
+
+//var myStr = "This is the start. " + "This is the end."
+//console.log(myStr);
+
+//concatenating strings with plus equal operator +=
+
+//var myStr = "This is the first sentence. "
+//myStr += "This is the second sentence. "
+//console.log(myStr);
+
+//constructive strings with variables
+
+//var myName = "Fuad";
+//var myStr = "My name is " + myName + " and I am well!";
+//console.log(myStr);
+
+//Appending Variables to Strings
+
+//var anAdjective = "awesome!";
+//var ourStr = "freeCodeCamp is ";
+//ourStr += anAdjective;
+//console.log(ourStr)
+
+//var someAdjective = "worthwhile";
+//var myStr = "Learning to code is ";
+//myStr += someAdjective;
+//console.log(myStr)
+
+// Find lenght of a string
+//var firstNameLength = 0;
+//var firstName = "Ada";
+
+//firstNameLength = firstName.length;
+//console.log(firstNameLength);
+
+//var lastNameLength = 0;
+//var lastName = "Lovelace";
+//lastNameLength = lastName.length;
+//console.log(lastNameLength)
+
+// Bracket Notation to find first character in string
+
+//var firstLetterOfFirstName = "";
+//var firstName = "Ada";
+
+//firstLetterOfFirstName = firstName[0];
+//console.log(firstLetterOfFirstName)
+
+var firstLetterOfLastName = "";
+var lastname = "Lovelace";
+

82a72bd06b839aec9aa103e78cca537bb635ed27
fuad1706 <fuadshorlarxwix@gmail.com>
2024-01-27 15:13:36 -0800

    switch

--- tut.js
+++ tut.js
@@ -202,10 +202,10 @@ CODE OUTPUT
 //firstNameLength = firstName.length;
 //console.log(firstNameLength);
 
-//var lastNameLength = 0;
-//var lastName = "Lovelace";
-//lastNameLength = lastName.length;
-//console.log(lastNameLength)
+// var lastNameLength = 0;
+// var lastName = "Lovelace";
+// lastNameLength = lastName.length;
+// console.log(lastNameLength)
 
 // Bracket Notation to find first character in string
 
@@ -215,10 +215,10 @@ CODE OUTPUT
 //firstLetterOfFirstName = firstName[0];
 //console.log(firstLetterOfFirstName)
 
-//var firstLetterOfLastName = "";
-//var lastname = "Lovelace";
-//firstLetterOfLastName = [4];
-//console.log(firstLetterOfLastName);
+// var firstLetterOfLastName = "";
+// var lastname = "Lovelace";
+// firstLetterOfLastName = [4];
+// console.log(firstLetterOfLastName);
 
 // Bracket Notation to find last character in string
 //var firstName = "Ada";
@@ -252,11 +252,18 @@ CODE OUTPUT
 function worldBlanks(myNoun, myAdjective, myVerb, myAdverb) {
   var result = "";
   result +=
-    "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb;
+    "The " +
+    myAdjective +
+    " " +
+    myNoun +
+    " " +
+    myVerb +
+    " to the store " +
+    myAdverb;
   return result;
 }
 console.log(worldBlanks("dog", "big", "ran", "quickly"));
-console.log(worldBlanks("bike", "slow", "flew", "slowly"))
+console.log(worldBlanks("bike", "slow", "flew", "slowly"));
 
 // Store multiple values with arrays
 
@@ -265,49 +272,63 @@ console.log(ourArray);
 
 //Nested Array
 
-var ourArray = [["The universe", 42], ["everything", 101010]];
+var ourArray = [
+  ["The universe", 42],
+  ["everything", 101010],
+];
 
 console.log(ourArray);
 
 //Access Array Data with indexes
 
-var ourArray = [50,60,70];
+var ourArray = [50, 60, 70];
 var ourData = ourArray[0];
 
-var myArray = [50,60,70]
+var myArray = [50, 60, 70];
 var myData = myArray[0];
 console.log(myData);
 
 //Modify Array Data with indexes
-var ourArray = [18,64,99];
+var ourArray = [18, 64, 99];
 ourArray[1] = 45;
 console.log(ourArray);
 
-var myArray = [18,64,99];
+var myArray = [18, 64, 99];
 myArray[0] = 45;
-console.log(myArray)
+console.log(myArray);
 
 //Access Multi-domensional Arrays with indexes
 
-var myArray = [[1,2,3],[4,5,6],[7,8,9], [[10,11,12], 13, 14]];
-var myData = myArray[2][1];
+var myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+var myData = myArray[3][0];
 console.log(myData);
 
-// Manipulate Arrays with push()
+// Manipulate Arrays with push() add item to the end of an array
 var ourArray = ["Stimpson", "J", "cat"];
 ourArray.push(["happy", "joy"]);
 console.log(ourArray);
 
-var myArray = [["John", 23],["cat", 2]];
-myArray.push(["dog", 3])
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+myArray.push(["dog", 3]);
 console.log(myArray);
 
-//Manipulate Arrays with pop()
-var ourArray = [1,2,3];
+//Manipulate Arrays with pop() it remove data from the begining of an array
+var ourArray = [1, 2, 3];
 var removedFromOurArray = ourArray.pop();
 console.log(ourArray);
 
-var myArray = [["John", 23], ["cat", 2]];
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
 var removedFromMyArray = myArray.pop();
 console.log(myArray);
 
@@ -316,50 +337,61 @@ var ourArray = ["Stimpson", "J", ["cat"]];
 var removedFromOurArray = ourArray.shift();
 console.log(ourArray);
 
-var myArray = [["John", 23], ["dog", 3]];
+var myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
 var removedFromMyArray = myArray.shift();
 console.log(myArray);
 
-// Manipulate Arrayas with unshift() add to the begining of the array
+// Manipulate Arrays with unshift() add to the begining of the array
 var ourArray = ["Stimpson", "j", "cat"];
-ourArray.shift(); 
+ourArray.shift();
 ourArray.unshift("Happy");
 console.log(ourArray);
 
-var myArray = [["John", 23], ["dog", 3]];
+var myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
 myArray.shift();
-myArray.unshift(["Olofin", 208])
+myArray.unshift(["Olofin", 208]);
 console.log(myArray);
 
 // Shopping List
 
-var myList = [["cereal", 3], ["milk", 2], ["bananas", 3], ["juice", 2], ["eggs", 12]];
+var myList = [
+  ["cereal", 3],
+  ["milk", 2],
+  ["bananas", 3],
+  ["juice", 2],
+  ["eggs", 12],
+];
 
 // write reusable code with functions
 
 function ourReusableFunction() {
-
   console.log("Heyya, World");
 }
 
 ourReusableFunction();
 
-function reusableFunction(){
+function reusableFunction() {
   console.log("Hi World");
 }
 
 reusableFunction();
 
 // Passing Values to Functions with Arguments
-function ourFunctionWithArgs(a, b){
+function ourFunctionWithArgs(a, b) {
   console.log(a - b);
 }
 ourFunctionWithArgs(10, 5);
 
-function functionWithArgs(a, b){
-  console.log(a + b)
+function functionWithArgs(a, b) {
+  console.log(a + b);
 }
-functionWithArgs(10, 5)
+functionWithArgs(10, 5);
 
 //Global scope and functions
 
@@ -386,9 +418,397 @@ function addThree() {
   sum = sum + 3;
 }
 
-function addFive(){
+function addFive() {
   sum += 5;
 }
 
 //Assignment with a returned value
+
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+console.log(changed);
+
+var processed = 0;
+function processArg(num) {
+  return (num + 2) / 7;
+}
+
+processed = processArg(47);
+console.log(processed);
+
+//Stand in Line
+
+function nextInLine(arr, item) {
+  arr.push(item);
+  return arr.shift();
+}
+
+var testArr = [1, 2, 3, 4, 5];
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+//Boolean Values true or false
+
+//use conditional logic with if statements
+
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+}
+console.log(trueOrFalse(false));
+
+// comparison with the equality operator
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+console.log(testEqual(12));
+
+//comparison with the strict equality operator must be the number if not it will return false
+function testStrict(val) {
+  if (val === 7) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+console.log(testStrict(3));
+
+//Practice Comparing Different Values
+function compareEquality(a, b) {
+  if (a === b) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+console.log(compareEquality(10, "10"));
+
+//comparison with the inequality operator
+function testNotEqual(val) {
+  if (val != 99) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+console.log(testNotEqual(23));
+
+//compariosn with the strict inequality operator
+function testStrictNotEqual(val) {
+  if (val !== 17) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+console.log(testStrictNotEqual(17));
+
+//comparison with the logical and operator
+function testGreaterThan(val) {
+  if (val > 100) {
+    return "Over 100";
+  }
+  if (val > 10) {
+    return "Over 10";
+  }
+  return "10 or Under";
+}
+console.log(testGreaterThan(101));
+
+//comparison with greater than or equal to operator
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    return "20 or Over";
+  }
+  if (val >= 10) {
+    return "10 or Over";
+  }
+  return "Less than 10";
+}
+console.log(testGreaterOrEqual(22));
+
+//comparison with less than operator
+function testLessThan(val) {
+  if (val < 25) {
+    return "Under 25";
+  }
+  if (val < 55) {
+    return "Under 55";
+  }
+  return "55 or Over";
+}
+console.log(testLessThan(22));
+
+//comparison with less than or equal to operator
+function testLessOrEqual(val) {
+  if (val <= 12) {
+    return "Smaller Than or Equal to 12";
+  }
+  if (val <= 24) {
+    return "Smaller Than or Equal to 24";
+  }
+  return "More Than 24";
+}
+console.log(testLessOrEqual(50));
+
+//comparison with the logical and operator &&
+
+function testLogicalAnd(val) {
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+  return "No";
+}
+
+console.log(testLogicalAnd(27));
+
+// Comparison with the logical or Operator ||
+
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+console.log(testLogicalOr(5));
+
+//Else Statements
+function testElse(val) {
+  var result = "";
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  return result;
+}
+console.log(testElse(5));
+
+//Else if Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+console.log(testElseIf(4));
+
+//Logical order in if else statement
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+console.log(orderMyLogic(3));
+
+//Chaining If else statements
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+}
+console.log(testSize(200));
+
+//Golf code
+
+var names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home",
+];
+
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else if (strokes >= par + 3) {
+    return names[6];
+  }
+}
+console.log(golfScore(1, 3));
+
+/*
+Strokes     Return
+1           "Hole-in-one!"
+<= par -2   "Eagle"
+par - 1      "Birdie"
+par          " Par"
+par + 1      " Bogey"
+par + 2      " Double Bogey"
+>= par + 3    "Go Home!"
+*/
+
+//Switch statement
+function caseInSwitch(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+  }
+
+  return answer;
+}
+
+console.log(caseInSwitch(3));
+
+//Default option in switch statements
+
+function switchOfStuff(val) {
+  var answer = "";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+  return answer;
+}
+console.log(switchOfStuff("a"));
+
+//Multiple identical options in switch statements
+
+function sequentialSizes(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+console.log(sequentialSizes(3));
+
+// Replacing if else chain with switch
+function chainToSwitch(val) {
+  var answer = "";
+  /*if (val === "bob"){
+    answer = "Marley"
+  } else if (val === 42) {
+    answer = "The Answer"
+  }else if (val === 1){
+    answer = "There is no #1"
+  } else if (val === 99){
+    answer = "Missed me by this much!"
+  }else if (val === 7){
+    answer = "Ate Nine"
+  }
+*/
+
+  //replacement
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+  }
+  return answer;
+}
+console.log(chainToSwitch("bob"));
+
+// Returning boolean values from functions
+
+function isLess(a, b){
+  /*if (a > b){
+    return true
+  }else{
+    return false
+  }*/
+
+  // the code can also be written
+  return a < b;
+}
+console.log(isLess(10, 15));
+
+//Returning Early Pattern from function
+
+function abTest(a, b) {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(abTest(2,2));
+
+// Counting Cards
+
+
 
